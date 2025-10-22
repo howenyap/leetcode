c08457b54798e277526a0a870141d9544f70e69b
howenyap <wenhyap02@gmail.com>
2025-10-22 13:50:57 +0800

    20

---
+++ 0020-valid-parantheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+        lookup = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        }
+
+        for c in s:
+            if c in lookup:
+                stack.append(c)
+            elif not stack or lookup[stack.pop()] != c:
+                return False
+            
+        return not stack
+        
